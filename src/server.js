a2e43989e6d9d54cf79102b7dc2ea501042cee0b
Ronaldo Alves da Costa <ronaldo.alves.costa@gmail.com>
2019-10-02 15:38:50 -0300

    correcoes para adequecao ao dev frontend

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require ('mongoose');
 const routes = require('./routes');
-
+const cors = require('cors')
+const path = require('path')
 
 const app = express();
 
@@ -13,7 +14,10 @@ mongoose.connect('mongodb+srv://mongouser:SM5uzHntatnBAHgK@cluster0-asyfc.mongod
     useUnifiedTopology: true,
 
 })
+//app.use(cors({ origin: 'http://localhost:3333' }))
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3000);
+app.listen(3333);
